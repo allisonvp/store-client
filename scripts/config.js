c12ab329e973b7a6f0382df1063f6c2c233ac827
Allison <a.velardep@pucp.edu.pe>
2023-01-25 14:53:08 -0500

    update base_uri

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 //This variable stores the base url of the api
 //Api deployed in heroku
-const BASE_URI = 'https://api-bsaletest.herokuapp.com/';
+const BASE_URI = 'https://api-store-seven.vercel.app/';
 //Local API
 // const BASE_URI = 'http://localhost:3000/';
 export default BASE_URI;
